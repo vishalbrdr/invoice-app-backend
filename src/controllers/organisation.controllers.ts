e55317fb8ca23eaded4935e5f6bedb220f125304
vishalbrdr <biradarvishal290@gmail.com>
2024-01-30 21:55:52 +0530

    Add registration functionality for organisations

--- src/controllers/organisation.controllers.ts
+++ src/controllers/organisation.controllers.ts
@@ -1,7 +1,54 @@
 import { asyncHandler } from "../utils/asyncHandler";
 import { Organisation } from "../models/organisation.model";
+import { OrganisationType } from "../middlewares/organisation.middleware";
+import { User } from "../models/user.model";
+import { Address } from "../models/address.model";
+import { BankAccInfo } from "../models/bankAccInfo";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
 
 export const registerOrganisation = asyncHandler(async (req, res) => {
-  const organisation: object = req.body;
-  res.json(organisation);
+  const { name, owner, address, bankAccInfo, gstin } = <OrganisationType>(
+    req.body
+  );
+
+  // check if email already exists
+  const user = await User.findOne({ email: owner.email });
+  if (user)
+    return res
+      .status(400)
+      .json(
+        new ApiError(400, "", [
+          "email already registered with another organisation",
+        ])
+      );
+
+  const regUser = await User.create(owner);
+  if (!regUser) throw new ApiError(400, "");
+
+  const regAddress = await Address.create(address);
+  if (!regAddress)
+    return res.status(400).json("registration failed while adding address");
+
+  const regBankAccInfo = await BankAccInfo.create(bankAccInfo);
+  if (!regBankAccInfo)
+    return res.status(400).json("registration failed while adding BankAccInfo");
+
+  const organisation = await Organisation.create({
+    name,
+    owner: regUser._id,
+    address: regAddress._id,
+    bankAccInfo: regBankAccInfo._id,
+    gstin,
+  });
+
+  const orgInfo = await organisation.populate([
+    { path: "owner", select: "-password" },
+    "address",
+    "bankAccInfo",
+  ]);
+
+  return res.json(
+    new ApiResponse(200, orgInfo, "organisation registered successfully")
+  );
 });
--- src/middlewares/organisation.middleware.ts
+++ src/middlewares/organisation.middleware.ts
@@ -5,7 +5,7 @@ import { AddressSchema } from "./address.middleware";
 import { BankAccInfoSchema } from "./bankAccInfo.middleware";
 import { ApiError } from "../utils/ApiError";
 
-export const organisationSchema = z.object({
+export const OrganisationSchema = z.object({
   name: z.string().min(3).max(30),
   owner: UserSchema,
   address: AddressSchema,
@@ -13,8 +13,10 @@ export const organisationSchema = z.object({
   gstin: z.string().optional(),
 });
 
+export type OrganisationType = z.infer<typeof OrganisationSchema>;
+
 export const validateOrganisationData = asyncHandler(async (req, _, next) => {
-  const result = organisationSchema.safeParse(req.body);
+  const result = OrganisationSchema.safeParse(req.body);
 
   if (result.success === false)
     throw new ApiError(
